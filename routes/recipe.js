e85aa3c5d668af0b0072266f309f75b13c8ac61c
sem-a <aleksey.i99@mail.ru>
2025-03-15 12:15:22 +0300

    настроен роутинг для рецептов

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,9 +1,26 @@
 const express = require("express");
-const { addRecipe } = require("../controllers/recipe");
 const router = express.Router();
+const {
+  addRecipe,
+  getAllRecipes,
+  getRecipeForId,
+  editRecipe,
+  deleteRecipe,
+} = require("../controllers/recipe");
 
-router.get("/", function (req, res, next) {});
+// api/recipes/
+router.get("/", getAllRecipes);
 
+// api/recipes/:id
+router.get("/:id", getRecipeForId);
+
+// api/recipes/add
 router.post("/add", addRecipe);
+
+// api/recipes/edit/:id
+router.put("/edit/:id", editRecipe);
+
+// api/recipes/delete/:id
+router.delete("/delete/:id", deleteRecipe);
 
 module.exports = router;
